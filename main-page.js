5ad6215c2a06d22814a49ad2bb0bc96cbeaf2247
mysticsnk <smachny.b@gmail.com>
2025-04-13 09:41:55 +0300

    Delete main-page.js

--- main-page.js
+++
@@ -1,9 +0,0 @@
-function showBurgerMenu() {
-  const burgermenu = document.querySelector('.burger-menu-nav')
-  burgermenu.style.display = 'flex'
-}
-
-function hideBurgerMenu() {
-  const burgermenu = document.querySelector(".burger-menu-nav");
-  burgermenu.style.display = "none";
-}
