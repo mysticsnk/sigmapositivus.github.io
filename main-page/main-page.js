64fd05a63afd5ef7e79a8e2e89993055e10e93b8
mysticsnk <smachny.b@gmail.com>
2025-04-13 09:42:26 +0300

    Add files via upload

---
+++ main-page/main-page.js
@@ -0,0 +1,9 @@
+function showBurgerMenu() {
+  const burgermenu = document.querySelector('.burger-menu-nav')
+  burgermenu.style.display = 'flex'
+}
+
+function hideBurgerMenu() {
+  const burgermenu = document.querySelector(".burger-menu-nav");
+  burgermenu.style.display = "none";
+}
